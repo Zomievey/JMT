f12cbfb59ab3d482b1d329c51dd36c10c9cab9a3
Haylee Hunter <haylee_hunter@aol.com>
2021-09-29 22:03:01 -0500

    updated JS

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-    //set sentences as letiables
-    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+    //set sentences as variables
+    var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
      'Too ato too nOt enot one totA not anot tOO aNot', 
      'oat itain oat tain nate eate tea anne inant nean', 
      'itant eate anot eat nato inate eat anot tain eat', 
      'nee ene ate ite tent tiet ent ine ene ete ene ate'];
     //set word container to the first place in the sentences array on load
-    let sentenceIndex = 0;
+    var sentenceIndex = 0;
 
-    let letterIndex = 0;
-    let currentLetterDiv = $("#next-letter");
-    currentLetterDiv.text(currentLetter);
+    var varterIndex = 0;
+    var currentvarterDiv = $("#next-varter");
+    currentvarterDiv.text(currentvarter);
     $("#words").append(sentences[sentenceIndex]);
 
-    let currentSentence = sentences[0];
-    let currentLetter = currentSentence[0];
-    let start; // for timer
-    let finish;// for timer
-    let errors = 0; // to track mistakes
+    var currentSentence = sentences[0];
+    var currentvarter = currentSentence[0];
+    var start; // for timer
+    var finish;// for timer
+    var errors = 0; // to track mistakes
 
     $("#sentence").append(currentSentence); // setting the content of the sentence div to = currentSentence
-    $("#next-letter").append(currentLetter); // setting the content of the next-letter div to = currentLetter
+    $("#next-varter").append(currentvarter); // setting the content of the next-varter div to = currentvarter
     //make the uppercase keyboard hidden on page load
     $("#keyboard-upper-container").hide(); // hides the uppercase keyboard
 
     $(document).keydown(function (event) { //show uppercase keyboard and hide lowercase keyboard on shift key press
-        let keyDown = event.which;
+        var keyDown = event.which;
         if (keyDown === 16) {
             $("#keyboard-upper-container").show();
             $("#keyboard-lower-container").hide();
@@ -34,7 +34,7 @@ $(document).ready(function () {
 
     //hide uppercase keyboard on shift release and show lowercase
     $(document).keyup(function (event) {
-        let keyUp = event.which;
+        var keyUp = event.which;
         if (keyUp === 16) {
             $("#keyboard-upper-container").hide();
             $("#keyboard-lower-container").show();
@@ -44,10 +44,10 @@ $(document).ready(function () {
 
     //highlight keys on press
     $(document).keypress(function (event) {
-        let keyPress = event.which;
+        var keyPress = event.which;
         $('#' + keyPress).addClass('highlight');
-        let currentSentence = sentences[sentenceIndex];
-        let currentLetter = currentSentence[letterIndex];
+        var currentSentence = sentences[sentenceIndex];
+        var currentvarter = currentSentence[varterIndex];
 
         if (start == undefined) { // if there is no value in start, set it to = the event.timeStamp
             start = event.timeStamp;
@@ -55,12 +55,12 @@ $(document).ready(function () {
 
         $("#highlightBlock").css("left", "+=17.5px"); //moves the highlightBlock to the left by offsetting it to the right
 
-        letterIndex++; //advances the letterIndex by 1
-        let nextLetter = currentSentence[letterIndex]; // sets nextLetter = the current letterIndex
-        currentLetterDiv.text(nextLetter); //sets the content of currentLetterDiv = nextLetter
+        varterIndex++; //advances the varterIndex by 1
+        var nextvarter = currentSentence[varterIndex]; // sets nextvarter = the current varterIndex
+        currentvarterDiv.text(nextvarter); //sets the content of currentvarterDiv = nextvarter
 
-        if (letterIndex < currentSentence.length -1) { // if letterIndex hasn't gone through the entire word yet
-            if (event.which === currentLetter.charCodeAt()) { // if key pressed is correct
+        if (varterIndex < currentSentence.length -1) { // if varterIndex hasn't gone through the entire word yet
+            if (event.which === currentvarter.charCodeAt()) { // if key pressed is correct
                 $("#feedback").append("<span class = 'glyphicon glyphicon-ok'></span>"); //set ok icon
             } else {
                 $("#feedback").append("<span class = 'glyphicon glyphicon-remove'></span>"); //if incorrect set icon to X
@@ -68,30 +68,30 @@ $(document).ready(function () {
             }
         }
 
-        if (letterIndex == currentSentence.length) { // if letterIndex = the end of the currentSentence
+        if (varterIndex == currentSentence.length) { // if varterIndex = the end of the currentSentence
             $("#sentence").empty(); //get the sentence div and empty it
             sentenceIndex++; //advance sentenceIndex by 1
             currentSentence = sentences[sentenceIndex]; //set currentSentence = the sentence in teh sentences array 
             //at position sentenceIndex
             $("#sentence").append(sentences[sentenceIndex]); // set content of #sentence to the current sentence 
-            letterIndex = 0; //reset the letter index to 0 to start at the beginning of the new sentence
+            varterIndex = 0; //reset the varter index to 0 to start at the beginning of the new sentence
             if (sentenceIndex < sentences.length - 1) { 
-                let nextLetter = currentSentence[letterIndex];
+                var nextvarter = currentSentence[varterIndex];
             }
-            currentLetterDiv.text(nextLetter); // set the content of the currentLetterDiv = nextLetter
+            currentvarterDiv.text(nextvarter); // set the content of the currentvarterDiv = nextvarter
             $("#highlightBlock").css({ left: 17 }); //resets the position of the highlightBlock
             $("#feedback").empty(); //clears the feedback div
         }
 
         if (sentenceIndex > sentences.length - 1) { //if you're all the way through the sentences array
             finish = event.timeStamp; //set finish timer to that timer
-            let time = (finish - start); //set time = difference between finish and start timeStamp
+            var time = (finish - start); //set time = difference between finish and start timeStamp
             time /= 60000; //divide timestamp of finish by 60000
-            let speed = Math.round((54 / time) - (errors * 2)); // divides time by # of words in test and subtracts errors * 2
-            $("#next-letter").text("Your score is " + speed + " words per minute"); //setting content of next-letter div to results of test
+            var speed = Math.round((54 / time) - (errors * 2)); // divides time by # of words in test and subtracts errors * 2
+            $("#next-varter").text("Your score is " + speed + " words per minute"); //setting content of next-varter div to results of test
 
             setTimeout(function () {
-                let tryAgain = confirm("Do you wnat to try again?");
+                var tryAgain = confirm("Do you wnat to try again?");
                 if (tryAgain == true) {
                     window.location.reload(); 
                     return;
